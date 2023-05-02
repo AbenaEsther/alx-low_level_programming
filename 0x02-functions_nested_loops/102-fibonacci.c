950494ca30f0198b30e56bf7b290ad42977b1bd0
AbenaEsther <essieofosina78@gmail.com>
2023-05-02 07:58:05 -0700

    1 and 2 fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * main - Prints the first 52 fibonacci numbers
+  *
+  * Return: Nothing!
+  */
+
+int main(void)
+{
+    int num1 = 1, num2 = 2, temp;
+
+    printf("%d\n%d\n", num1, num2);
+    
+    for (int i = 3; i <= 50; i++)
+    {
+        temp = num1 + num2;
+        printf("%d\n", temp);
+        num1 = num2;
+        num2 = temp;
+    }
+    
+    return (0);
+}
