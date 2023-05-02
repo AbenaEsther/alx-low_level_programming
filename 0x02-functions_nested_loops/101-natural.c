fe8b19ffdc61117c08df117e4d2a3f9ce4670192
AbenaEsther <essieofosina78@gmail.com>
2023-05-02 05:58:23 -0700

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,11 +8,10 @@
 int main(void)
 {
 	int sum = 0;
-	int i = 1; 
-	
+	int i = 1;
 	for (i = 0; i < 1024; i++)
 	{
-		if(i % 3 == 0 || i % 5 == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
 			sum += i;
 		}
