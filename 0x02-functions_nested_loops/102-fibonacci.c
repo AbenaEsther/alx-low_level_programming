7a7e891a4e2b5de467598e0b605e199bf5e558d6
AbenaEsther <essieofosina78@gmail.com>
2023-05-02 11:20:40 -0700

    1 and 2 fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,17 +8,14 @@
 
 int main(void)
 {
-    int num1 = 1, num2 = 2, i = 3, temp;
-
-    printf("%d\n%d\n", num1, num2);
-    
-    for (i = 3; i <= 50; i++)
-    {
-        temp = num1 + num2;
-        printf("%d\n", temp);
-        num1 = num2;
-        num2 = temp;
-    }
-    
-    return (0);
+	int num1 = 1, num2 = 2, i = 3, temp;
+	printf("%d\n%d\n", num1, num2);
+	for (i = 3; i <= 50; i++)
+	{
+		temp = num1 + num2;
+		printf("%d\n", temp);
+		num1 = num2;
+		num2 = temp;
+	}
+	return (0);
 }
