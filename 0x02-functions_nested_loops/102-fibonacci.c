2070cd6a5be974f27a0a81c8d376d1e1089447c9
AbenaEsther <essieofosina78@gmail.com>
2023-05-02 12:35:52 -0700

    1 and 2 fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,9 +11,10 @@ int main(void)
 	int count = 3;
 	long int first = 1, second = 2;
 	long next = first + second;
+
 	printf("%lu, ", first);
 	printf("%lu, ", second);
-	
+
 	while (count <= 50)
 	{
 		if (count == 50)
