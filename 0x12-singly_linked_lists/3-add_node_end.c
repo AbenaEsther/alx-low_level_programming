372d6af847d3f09fa84ce0aa798f592a1f9a2321
AbenaEsther <essieofosina78@gmail.com>
2023-06-02 19:42:00 -0700

    adding newnode at the end of the list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,38 +1,58 @@
-new_node = malloc(sizeof(list_t));
-if (new_node == NULL)
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - Adds a new node at the end of a list_t list.
+ * @head: Pointer to a pointer to the head of the list.
+ * @str: String to be duplicated and added as the new node's str field.
+ *
+ * Return: The address of the new element, or NULL if it failed.
+ */
+list_t *add_node_end(list_t **head, const char *str)
 {
-	/* Returns NULL if memory allocation fails */
-	return (NULL);
+	list_t *new_node, *temp;
+
+	if (str == NULL)
+		return (NULL);
+
+	/* creating a new node */
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		/* returns NULL if memory allocation fails */
+		return (NULL);
+	}
+
+	/* duplicates the input string */
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		/* freeing memory and return NULL if string duplication fails */
+		free(new_node);
+		return (NULL);
+	}
+
+	/* sets the length of the string */
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		/* If the list is empty, set new_node as the head */
+		*head = new_node;
+	}
+	else
+	{
+		/* traverse list to find the last node */
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
+
+		/* settingthe next pointer of the last node to the new node */
+		temp->next = new_node;
+	}
+
+	return (new_node);
 }
-
-/* Duplicates the input string */
-new_node->str = strdup(str);
-if (new_node->str == NULL)
-{
-	/* Frees memory and returns NULL if string duplication fails */
-	free(new_node);
-	return (NULL);
-}
-
-/* Setting the length of the string */
-new_node->len = strlen(str);
-new_node->next = NULL;
-
-if (*head == NULL)
-{
-	/* If the list is empty, set new_node as the head */
-	*head = new_node;
-}
-else
-{
-	/* Traverse the list to find the last node */
-	temp = *head;
-	while (temp->next != NULL)
-		temp = temp->next;
-
-	/* Set the next pointer of the last node to the new node */
-	temp->next = new_node;
-}
-
-return (new_node);
 
