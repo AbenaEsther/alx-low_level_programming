7bad4233c3f4575722340643df913c32b1fd6dd6
AbenaEsther <essieofosina78@gmail.com>
2023-06-02 17:53:46 -0700

    printing list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 	{
 		/* If the string in the current node is NULL, print (nil) */
 		if (h->str == NULL)
-			printf( "[0](nil)\n");
+			printf("[%d](nil)\n",0);
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
