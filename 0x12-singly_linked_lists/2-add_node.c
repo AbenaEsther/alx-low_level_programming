c744a34931c5691e1c67c99d8ce2b94183c914d9
AbenaEsther <essieofosina78@gmail.com>
2023-06-02 19:13:19 -0700

    adding a new node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,11 +3,11 @@
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a list_t list
- * @head: pointer to a pointer to the head of the list
- * @str: string to be duplicated and added as the new node's str field
+ * add_node - Adds a new node at the beginning of a list_t list.
+ * @head: Pointer to a pointer to the head of the list.
+ * @str: String to be duplicated and added as the new node's str field.
  *
- * Return: the address of the new element, or NULL if it failed
+ * Return: The address of the new element, or NULL if it failed.
  */
 list_t *add_node(list_t **head, const char *str)
 {
@@ -15,30 +15,31 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (str == NULL)
 		return (NULL);
-	/*create a new todo*/
 
+	/* Create a new node */
 	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL){
-		/*return null if memory allocation fails*/
+	if (new_node == NULL)
+	{
+		/* Return NULL if memory allocation fails */
 		return (NULL);
-        }
+	}
 
-	/*duplicate the input string */
+	/* Duplicate the input string */
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
-		/* free memory,returns null if string duplication fails */
+		/* Free memory and return NULL if string duplication fails */
 		free(new_node);
 		return (NULL);
 	}
 
-	/* setting the  length of the string */
+	/* Set the length of the string */
 	new_node->len = strlen(str);
 
-	/* setting next pointer of new_node to the current heare head */
+	/* Set next pointer of new_node to the current head */
 	new_node->next = *head;
 
-	/* head now pointing to the new node */
+	/* Head now points to the new node */
 	*head = new_node;
 
 	return (new_node);
