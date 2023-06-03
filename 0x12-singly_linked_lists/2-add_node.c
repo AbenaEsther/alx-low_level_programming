e2e4c4819e97606d1314e767cc3e41b77f4a5480
AbenaEsther <essieofosina78@gmail.com>
2023-06-02 18:57:52 -0700

    addin a new node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -24,7 +24,7 @@ list_t *add_node(list_t **head, const char *str)
         }
 
 	/*duplicate the input string */
-	new_no->str = strdup(str);
+	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		/* free memory,returns null if string duplication fails */
