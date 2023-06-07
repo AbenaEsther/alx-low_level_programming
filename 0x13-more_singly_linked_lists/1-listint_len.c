6269602f3ed75f7e86b84a10cbc61fce922369da
AbenaEsther <essieofosina78@gmail.com>
2023-06-07 05:42:33 -0700

    list length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - Returns the number of elements in a listint_t list.
+ *
+ * @h: List to count the elements in.
+ *
+ * Return: Number of elements in the list.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return count;
+}
+
