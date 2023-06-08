f30d4e17b7d4af9f4e8ac76a4384babe96770a6e
AbenaEsther <essieofosina78@gmail.com>
2023-06-07 17:02:52 -0700

    print list safe version

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,35 +1,6 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
-
-/**
- * _realloc - Reallocates memory for the nodes in a linked list.
- * @l: The list to append.
- * @s: Size of the new list.
- * @n: New node to add.
- *
- * Return: Pointer to the new list.
- */
-const listint_t **_realloc(const listint_t **l, size_t s, const listint_t *n)
-{
-	const listint_t **list;
-	size_t i;
-
-	list = malloc(s * sizeof(listint_t *));
-	if (list == NULL)
-	{
-		free(l);
-		exit(98);
-	}
-
-	for (i = 0; i < s - 1; i++)
-		list[i] = l[i];
-
-	list[i] = n;
-	free(l);
-
-	return (list);
-}
 
 /**
  * print_listint_safe - Prints a listint_t linked list.
@@ -39,28 +10,25 @@ const listint_t **_realloc(const listint_t **l, size_t s, const listint_t *n)
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t a, len = 0;
-	const listint_t **l = NULL;
+	size_t count = 0;
+	const listint_t *current = head;
+	const listint_t *temp = NULL;
 
-	while (head != NULL)
+	while (current != NULL)
 	{
-		for (a = 0; a < len; a++)
+		count++;
+		printf("[%p] %d\n", (void *)current, current->n);
+		temp = current->next;
+
+		if (temp >= current)
 		{
-			if (head == l[a])
-			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				free(l);
-				return (len);
-			}
+			printf("-> [%p] %d\n", (void *)temp, temp->n);
+			break;
 		}
 
-		len++;
-		l = _realloc(l, len, head);
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
+		current = temp;
 	}
 
-	free(l);
-	return (len);
+	return (count);
 }
 
